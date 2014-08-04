90ebcfbcf18947101128c970848287652eca3bd2
Atul Varma <varmaa@gmail.com>
2014-08-04 07:34:30 -0400

    made new package, yt2mp3, and moved app.py to it.

--- test.py
+++ test.py
@@ -1,21 +1,21 @@
 import os
 import unittest
 
-import app
+import yt2mp3
 
 VALID_URL = 'https://www.youtube.com/watch?v=jNQXAC9IVRw'
 INVALID_URL = 'https://www.youtube.com/watch?v=INVALID'
 
 class TestIsLinkValid(unittest.TestCase):
     def test_returns_true(self):
-        self.assertTrue(app.is_link_valid(VALID_URL))
+        self.assertTrue(yt2mp3.is_link_valid(VALID_URL))
 
     def test_returns_false(self):
-        self.assertFalse(app.is_link_valid(INVALID_URL))
+        self.assertFalse(yt2mp3.is_link_valid(INVALID_URL))
 
 class TestDownloadLink(unittest.TestCase):
     def test_file_is_deleted_after_leaving_with(self):
-        with app.download_link(VALID_URL) as filename:
+        with yt2mp3.download_link(VALID_URL) as filename:
             self.assertTrue(os.path.exists(filename))
         self.assertFalse(os.path.exists(filename))
 
